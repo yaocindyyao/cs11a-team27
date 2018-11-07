b4c3607fb5e53d7d18b7cf6a72f93bc8a7974767
RSorliHawk <rsorlihawk@gmail.com>
2018-11-06 19:17:04 -0500

    Update MinimalViableProduct.java

---
+++ MinimalViableProduct.java
@@ -0,0 +1,8 @@
+public class MinimalViableProduct{
+  public static void main(String[] args){
+    System.out.println("Hello!! Welcome to Team 27's Game!");
+    System.out.print("Please enter your username:");
+    String username = TextIO.getlnString();
+    System.out.printf("Welcome, "+username"!");
+  }
+}
